673a0866869eba1deffdef434840c69b82e53310
erger565 <94860476+erger565@users.noreply.github.com>
2022-02-03 12:00:40 -0500

    Update GLTFLoader.js

--- js/three/GLTFLoader.js
+++ js/three/GLTFLoader.js
@@ -1,10 +1,4 @@
-/**
- * @author Rich Tibbett / https://github.com/richtr
- * @author mrdoob / http://mrdoob.com/
- * @author Tony Parisi / http://www.tonyparisi.com/
- * @author Takahiro / https://github.com/takahirox
- * @author Don McCurdy / https://www.donmccurdy.com
- */
+
 
 import {
 	AnimationClip,
